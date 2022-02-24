f4d16f5e4051f689a0d4cd5aaaa8a3571606f7b6
Paul Golter <paul@blender.org>
2022-02-24 22:04:48 +0100

    Add more packages to PACKAGES.sh

---
+++ .config/PACKAGES.sh
@@ -0,0 +1,44 @@
+# Neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt install neovim
+
+# Install language servers.
+sudo apt install clang
+sudo apt install clangd
+# Python language server for vim.
+sudo snap install pylsp
+
+# Fish shell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt install fish
+
+# Terminal Emulator
+sudo apt install kitty
+# sudo update-alternatives --config x-terminal-emulator
+
+sudo apt install neofetch
+
+# Window manager
+sudo apt install i3
+
+# Window compositor
+sudo apt install compton
+
+# Bars
+sudo apt install polybar
+
+# THEMES AND FONTS
+# --------------------------
+
+# Download Nerd Fonts.
+# git clone https://github.com/ryanoasis/nerd-fonts.git ~/dev/build/nerd-fonts
+
+# Gnome tweak
+# sudo apt install gnome-tweaks
+
+# Dracula
+# https://draculatheme.com/gtk
+# Download GTK dracula theme: https://github.com/dracula/gtk/tags move to:  sudo mv Downloads/dracula-theme /usr/share/themes/
