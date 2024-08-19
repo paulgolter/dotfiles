775d2fee56fe7d2d5765210dac7a46653493e6bf
Paul Golter <55129456+paulgolter@users.noreply.github.com>
2024-08-19 16:49:03 +0200

    add default powershell profile

---
+++ .config/windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+# Put this file in: $profile (%USERPFOFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1)
+Import-Module PSReadLine
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+# Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+# Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWor
+
