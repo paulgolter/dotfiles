31e14cd0b9a8ebe45653caccd6481fd5a39e8611
Paul Golter <55129456+paulgolter@users.noreply.github.com>
2024-08-19 14:05:43 +0200

    add new .install dir
    - add basic windows.ps1 script
    - move PACKAGES.sh to ubuntu.sh

---
+++ .install/windows.ps1
@@ -0,0 +1,51 @@
+# Install latest power shell
+winget install --id Microsoft.Powershell --source winget
+
+# Install 'Windows Terminal' from store 
+# https://apps.microsoft.com/detail/9n0dx20hk701?rtc=1&hl=de-de&gl=DE
+
+# Install pyenv
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+
+# Choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Scoop
+# If scoop installed as admin refer: https://github.com/ScoopInstaller/Install?tab=readme-ov-file#for-admin
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+# Non admin:
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# Pipx
+scoop install pipx
+pipx ensurepath
+
+# Poetry 
+pipx install poetry
+pipx ensurepath
+
+# Vscode 
+winget install -e --id Microsoft.VisualStudioCode
+
+# Git 
+winget install -e --id Git.Git
+
+# Neovim
+winget install Neovim.Neovim
+
+# WSL 
+# Open a admin power shell and run
+wsl --install
+# --> needs reboot
+
+# Blender
+winget install -e --id BlenderFoundation.Blender
+
+# Path Copy
+# https://github.com/clechasseur/pathcopycopy/releases
+
+# Maxon App
+# Autodesk Access
+# SideFX Launcher
+# Adobe Creative Cloud
