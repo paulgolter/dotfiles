a5ed2662d06cee440e3ddcd9ad0af23195ea0550
Paul Golter <paulgolter.de@gmail.com>
2025-08-31 12:06:50 +0200

    Add mac install packages

---
+++ .install/mac.sh
@@ -0,0 +1,8 @@
+# CLI packages refer to ubuntu.sh
+
+
+# Swish https://highlyopinionated.co/swish/
+
+# Brew cask packages
+brew install --cask anydesk docker karabiner-elements	obs	visual-studio-code
+blender docker-desktop middleclick slack warp discord	google-chrome	nordvpn	spotify
